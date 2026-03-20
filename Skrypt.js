b90afd583c8fb74f201de7eb1e8ec26665f49a1a
Adrian Rauczyński <rauczynskiadrian@gmail.com>
2026-03-20 22:32:46 +0100

    zrobienie strony początkowej

---
+++ Skrypt.js
@@ -0,0 +1,11 @@
+function httml(){
+window.location.href = "html/html.html"
+}
+
+function ccss(){
+window.location.href = "css/css.html"
+}
+
+function jjava(){
+window.location.href = "js/js.html"
+}
